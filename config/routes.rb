7d33d24a5a1c3f144a355e2a28a9a3f6abcc12fa
mbm_1607 <muhammadkhan1607@gmail.com>
2021-05-03 15:08:47 +0500

    Create Project Skeleton
    Add Devise and Bulma gems. Create homepage.
    Create quiz data models and resources.
    Create configuration for future.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :results
+  resources :answers
+  resources :questions
+  resources :quizzes
+  devise_for :users
+
+  root 'quizzes#index'
 end
